4474239214c5008b94ffc25ad1efbfa7dd028c26
GVill683 <pandolfogabriel@gmail.com>
2017-10-18 17:54:30 -0700

    Complete homework 7

--- hw07/js/index.js
+++ hw07/js/index.js
@@ -6,8 +6,30 @@
 // parameter. In the body of the function, there should be code that
 // appends an "li" element, with the string inside, to the un-ordered
 // list ("ul" tag):
+const addlistitemToUnorderedHTMLLIST = (message) => {
 
+document.getElementById('output').innerHTML += "<li> "+ message +" </li>"
 
+//step 1: get the element in the HTML file:
+const ul = document.getElementById('output')
+
+//step 2: create a brand new li tag
+const li = document.createElement("li")
+
+//step 3: createTextNode with the message inside
+const textNode = document.createTextNode(message)
+
+//step 4: append the textNode to the li element
+li.appendChild(textNode)
+
+//step 5: append the li element to the ul element
+ul.appendChild(li)
+
+}
+
+//addlistitemToUnorderedHTMLLIST("Steve")
+//addlistitemToUnorderedHTMLLIST("Alex")
+//addlistitemToUnorderedHTMLLIST("Karin")
 
 //------------------------------------------------------------------
 // PART II
@@ -19,3 +41,17 @@
 // 2. If the number is divisible by 5, append "Buzz" to the unordered list
 // 3. If the number is divisible by 3 AND 5, append "FizzBuzz" to the unordered list.
 // 4. Bonus points: Include both the number *and* the word you've appended to the list
+for (let i = 1; i<=100; i++) {
+
+
+  if ( i % 3 === 0 &&  i % 5 === 0) {
+    addlistitemToUnorderedHTMLLIST("FizzBuzz")
+} else if (i % 3 === 0) {
+    addlistitemToUnorderedHTMLLIST("Fizz")
+} else if (i % 5 === 0) {
+    addlistitemToUnorderedHTMLLIST("Buzz")
+} else  {
+    addlistitemToUnorderedHTMLLIST(i)
+}
+
+}
