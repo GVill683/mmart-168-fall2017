ee77107b4bac34ae1b68eb1bf589ac64e41e7402
GVill683 <pandolfogabriel@gmail.com>
2017-11-14 23:31:13 -0800

    Completed homework 10

--- hw10/js/main.js
+++ hw10/js/main.js
@@ -1,6 +1,8 @@
 const apiKey = 'MW9S-E7SL-26DU-VV8V'
 
 const makeStationList = () => {
+//url that asks the question you want to you ask. In this case, give me
+//a list of all of the Bart stations:
     const url = 'https://api.bart.gov/api/stn.aspx?key=' + apiKey +
                     '&cmd=stns&json=y'
     fetch(url)
@@ -8,26 +10,40 @@ const makeStationList = () => {
             return response.json()
         })
         .then((json) => {
+          //Do the thing!
             json = json.root
             console.log(json)
+            json.stations.station.forEach((station) => {
+              const option = document.createElement("option")
 
+              option.innerHTML = station.name
+              option.value = station.abbr
+
+              document.getElementById('station_list').appendChild(option)
+          })
+
+            /*
             // PART III.A.: Use a loop to populate the select menu with *ALL*
             // of the stations that are returned from the BART data feed:
             const option1 = document.createElement("option")
             option1.value = 'DBRK'
             option1.innerHTML = 'Downtown Berkeley'
             document.getElementById('station_list').appendChild(option1)
+            */
         })
         .catch((err) => {
             console.log(err)
         })
-}
+      }
 
 const getArrivalTimes = () => {
+  // go out and find the element witht he stationList (element), storing it in a variable called stationList
     const stationList = document.getElementById('station_list')
     // PART III.B.1: The bartStationCode should read from the list and query
     // for the corresponding station
-    const bartStationCode = 'DBRK'
+    const bartStationCode = stationList.value
+
+    console.log (bartStationCode)
     console.log('Selected Station Code:', bartStationCode)
     let url = 'https://api.bart.gov/api/etd.aspx?key=' + apiKey + '&cmd=etd' +
                 '&orig=' + bartStationCode + '&json=y'
@@ -36,8 +52,65 @@ const getArrivalTimes = () => {
             return response.json()
         })
         .then((json) => {
-            json = json.root
-            console.log(json)
+          json = json.root
+
+
+          // clear out existing div
+          document.getElementById('results').innerHTML = ''
+
+          //add header that shows the selected station name
+          const header = document.createElement("h2")
+          header.innerHTML =  json.station[0].name
+          document.getElementById('results').appendChild(header)
+
+          // log all of the train lines:
+          json.station[0].etd.forEach((line) => {
+
+            const trainLine = document.createElement("h3")
+            trainLine.innerHTML =  line.destination
+
+            trainLine.innerHTML += ':Platform #' + line.estimate [0].platform
+
+            trainLine.innerHTML +=  ' (' + line.estimate [0].direction + ')'
+
+            document.getElementById('results').appendChild(trainLine)
+
+            const square = document.createElement("span")
+            square.style.background = line.estimate [0].hexcolor
+
+            document.getElementById('results').appendChild(square)
+
+            //log all the estimates for each train line:
+          const departureTimes = []
+          line.estimate.forEach((estimate) => {
+              departureTimes.push(estimate.minutes)
+              console.log('Estimate', estimate)
+            })
+
+
+            const departureTime = document.createElement("span")
+            departureTime.innerHTML = departureTimes.join (', ')
+            departureTime.innerHTML += 'minutes'
+            document.getElementById('results').appendChild(departureTime)
+
+          })
+
+        })
+
+  }
+
+    /*
+    console.log('Selected Station Code:', bartStationCode)
+    let url = 'https://api.bart.gov/api/etd.aspx?key=' + apiKey + '&cmd=etd' +
+                '&orig=' + bartStationCode + '&json=y'
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+          json = json.root
+          console.log(json)
+
             const results = document.getElementById('results')
             results.innerHTML = ''
             json.station = json.station[0]
@@ -70,6 +143,6 @@ const getArrivalTimes = () => {
         .catch((err) => {
             console.log(err)
         })
-}
+        */
 
 makeStationList()
